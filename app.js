0c3564b6505cae6bcd47c5ba24dc9f947d6f6e14
Kane <ori.lowen@gmail.com>
2023-06-11 09:51:12 -1000

    Update css variables. Replace operator if statements with switch statement

--- app.js
+++ app.js
@@ -4,24 +4,32 @@ let num1;
 let operator;
 let num2;
 
+// Create the functions that populate the display when you click the number buttons.
+// You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
 
 // FUNCTIONS:
 
 // OPERATE FUNCTION
 function operate(num1, num2, operator){
-    if (operator === "+") {
-        return add(num1, num2);
-    };
-    if (operator === "-") {
-        return subtract(num1, num2);
-    };
-    if (operator === "*") {
-        return multiply(num1, num2);
-    };
-    if (operator === "/") {
-        return divide(num1, num2);
-    };
+    num1 = Number(num1);
+    num2 = Number(num2);
+    
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            if (num2 === 0) return null;
+            else return divide(num1, num2);
+        default:
+            return null;
+    }
 };
+
 // ADDITION FUNCTION
 function add(num1, num2) {
     return num1 + num2;
